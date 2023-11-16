7e1e3e303f4c549adce9692460a6abcc78851a77
Harish Bhalaa <107175621+harish-bhalaa-07@users.noreply.github.com>
2023-11-16 23:06:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-from flask import Flask, jsonify
+import numpy as np
+from flask import Flask,request,render_template
+import pickle
 import os
 
-app = Flask(__name__)
+os.chdir('./')
+
+app=Flask(__name__)
+model=pickle.load(open('credit.pkl','rb'))
 
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def home():
+    return render_template('credit_score_prediction.html')  #html page here
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    #storing the input values from html form
+    features=[[int(x) for x in request.form.values()]]
+    final_features=np.array(features)  
+    
+    print(final_features)
+
+    #prediction=model.predict(final_features)
+    prediction = model.predict(final_features)
+    prediction=prediction.tolist()
+    
+    if prediction == [1]:
+        prediction = "Don't worry! Your credit bills are perfect"
+    else:
+        prediction = "You have to clear your pending credit card money to keep your credit score high"
+        
+
+    return render_template('credit_score_prediction.html', prediction_value = prediction)
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
